e134937d27265a0bebf4d7875f4620e1ab72013b
Hila Menashe <hilam@microsoft.com>
2023-01-17 17:05:01 +0200

    challenge 7

--- api/CustomerAPI/ValidationAttirbutes/AddressAttribute.cs
+++ api/CustomerAPI/ValidationAttirbutes/AddressAttribute.cs
@@ -27,8 +27,8 @@ namespace CustomerAPI.ValidationAttirbutes
 
             var country = customerCreateDto.Address.Country.ToLower();
 
-            if (country != "sweden" && country != "denmark" && country != "norway" && country != "finland")
-                return new ValidationResult("Country should be among (Sweden, Denmark, Norway, Finland)", new[] { "CustomerCreateDto" });
+            if (country != "sweden" && country != "denmark" && country != "norway" && country != "finland" && country != "iceland")
+                return new ValidationResult("Country should be among (Sweden, Denmark, Norway, Finland, Iceland)", new[] { "CustomerCreateDto" });
 
             var phoneNumber = customerCreateDto.PhoneNumber;
 
@@ -40,6 +40,8 @@ namespace CustomerAPI.ValidationAttirbutes
                 return new ValidationResult("Since the country is Norway, Phone Number should start with +47", new[] { "CustomerCreateDto" });
             else if (country == "finland" && !phoneNumber.StartsWith("+358"))
                 return new ValidationResult("Since the country is Finland, Phone Number should start with +358", new[] { "CustomerCreateDto" });
+            else if (country == "iceland" && !phoneNumber.StartsWith("+354"))
+                return new ValidationResult("Since the country is Iceland, Phone Number should start with +354", new[] { "CustomerCreateDto" });
 
             return ValidationResult.Success;
 
